36ec6d80da35c5147b3b632efb332499d0440f74
mahmoud-teir <mahmoudteirbusiness@gmail.com>
2026-01-29 11:49:31 +0200

    feat: add Content-Security-Policy header

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,10 @@ const nextConfig: NextConfig = {
             key: "Referrer-Policy",
             value: "strict-origin-when-cross-origin",
           },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src * blob: data:; worker-src 'self' blob:; connect-src *; frame-ancestors 'none';",
+          },
         ],
       },
     ];
